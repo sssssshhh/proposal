fe87e7e81bcc9655881cfe1e0ef67ddfad0e7684
sssssshhh <eyesh0011@gmail.com>
2023-02-13 21:17:33 +0900

    20230213_Create Basic Components

--- proposal-lw/src/app/app.module.ts
+++ proposal-lw/src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { Test1Component } from './application/test1/test1.component';
+import { Test2Component } from './application/test2/test2.component';
+import { Test3Component } from './application/test3/test3.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { MainComponent } from './navigation/main/main.component';
+import { LoginComponent } from './login/login/login.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Test1Component,
+    Test2Component,
+    Test3Component,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
